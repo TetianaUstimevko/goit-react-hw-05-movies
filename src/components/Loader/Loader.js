35313e2124e2760841e9cc787f14af6e27a07b87
TetianaUstimevko <107365766+TetianaUstimevko@users.noreply.github.com>
2023-05-25 20:11:34 +0300

    add js and css

--- src/components/Loader/Loader.js
+++ src/components/Loader/Loader.js
@@ -1,10 +1,10 @@
-// import MoonLoader from 'react-spinners/MoonLoader';
-// import s from './Loader.module.css';
+import MoonLoader from 'react-spinners/MoonLoader';
+import s from './Loader.module.css';
 
-// export default function Loader() {
-//   return (
-//     <div className={s.overlay}>
-//       <MoonLoader size={250} color={'#0c3c94de'} className={s.loader} />
-//     </div>
-//   );
-// }
+export default function Loader() {
+  return (
+    <div className={s.overlay}>
+      <MoonLoader size={250} color={'#0c3c94de'} className={s.loader} />
+    </div>
+  );
+}
