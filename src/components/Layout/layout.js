60952cda7d1f5f2d1b51b60bb2c64f7ad407d054
TetianaUstimevko <107365766+TetianaUstimevko@users.noreply.github.com>
2023-06-03 13:14:29 +0300

    fixed bugs

--- src/components/Layout/layout.js
+++ src/components/Layout/layout.js
@@ -1,15 +1,15 @@
 import { NavLink, Outlet } from 'react-router-dom';
-import s from './Layout.module.css'
+
 
 const Layout = () => {
   return (
     <div>
       <header>
         <ul>
-          <li className={s.navLink} activeClassName={s.active}>
+          <li >
             <NavLink to="/" >Home</NavLink>
           </li>
-          <li className={s.navLink} activeClassName={s.active}>
+          <li >
             <NavLink to="/movies" >Movies</NavLink>
           </li>
         </ul>
